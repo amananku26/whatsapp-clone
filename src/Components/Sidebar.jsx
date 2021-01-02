572ff8b0e6d3868390ca6747b4b73030b67fd490
AMAN ANKU <amankumaranku@gmail.com>
2021-01-02 21:42:55 +0530

    started working on header

--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -1,10 +1,32 @@
 import React from 'react'
 import "./Sidebar.css"
+import SettingsIcon from '@material-ui/icons/Settings';
+import { Avatar } from '@material-ui/core';
+import DonutLargeIcon from '@material-ui/icons/DonutLarge';
+import ChatIcon from '@material-ui/icons/Chat';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
 
 function Sidebar() {
     return (
         <div className="sidebar">
-            <h1>this is sidebar</h1>
+            <div className="sidebar__header">
+                <Avatar />
+                <div className="sidebar__headerRight">
+                    <DonutLargeIcon />
+                    <ChatIcon />
+                    <MoreVertIcon />
+                </div>
+
+            </div>
+            <div className="sidebar__search">
+
+
+            </div>
+
+            <div className="sidebar__chats">
+
+
+            </div>
         </div>
     )
 }
