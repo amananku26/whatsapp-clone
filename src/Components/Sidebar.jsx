5f38799fac6d77651dd9268a8c2236ebf6c6ac86
AMAN ANKU <amankumaranku@gmail.com>
2021-01-02 22:08:52 +0530

    sidebar updates

--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import "./Sidebar.css"
 import SettingsIcon from '@material-ui/icons/Settings';
-import { Avatar } from '@material-ui/core';
+import { Avatar, IconButton } from '@material-ui/core';
 import DonutLargeIcon from '@material-ui/icons/DonutLarge';
 import ChatIcon from '@material-ui/icons/Chat';
 import MoreVertIcon from '@material-ui/icons/MoreVert';
@@ -12,11 +12,16 @@ function Sidebar() {
             <div className="sidebar__header">
                 <Avatar />
                 <div className="sidebar__headerRight">
-                    <DonutLargeIcon />
-                    <ChatIcon />
-                    <MoreVertIcon />
+                    <IconButton>
+                        <DonutLargeIcon />
+                    </IconButton>
+                    <IconButton>
+                        <ChatIcon />
+                    </IconButton>
+                    <IconButton>
+                        <MoreVertIcon />
+                    </IconButton>
                 </div>
-
             </div>
             <div className="sidebar__search">
 
