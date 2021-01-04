49000d9a60a66699ad6edb2786fb04808119e9a8
AMAN ANKU <amankumaranku@gmail.com>
2021-01-04 20:33:56 +0530

    complete sidebar

--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -5,6 +5,8 @@ import { Avatar, IconButton } from '@material-ui/core';
 import DonutLargeIcon from '@material-ui/icons/DonutLarge';
 import ChatIcon from '@material-ui/icons/Chat';
 import MoreVertIcon from '@material-ui/icons/MoreVert';
+import SearchOutlinedIcon from '@material-ui/icons/SearchOutlined';
+import SidebarChat from './SidebarChat';
 
 function Sidebar() {
     return (
@@ -24,13 +26,15 @@ function Sidebar() {
                 </div>
             </div>
             <div className="sidebar__search">
-
-
+                <div className="sidebar__searchContainer">
+                    <SearchOutlinedIcon />
+                    <input placeholder="Search or Start new chats" />
+                </div>
             </div>
-
             <div className="sidebar__chats">
-
-
+                <SidebarChat />
+                <SidebarChat />
+                <SidebarChat />
             </div>
         </div>
     )
