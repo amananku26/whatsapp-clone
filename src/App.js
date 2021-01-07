5214a841823294995dff50cca67cc0ef986c59aa
AMAN ANKU <amankumaranku@gmail.com>
2021-01-07 09:41:00 +0530

    remove/bug the create unable to work Rout

--- src/App.js
+++ src/App.js
@@ -1,21 +1,34 @@
 import "./App.css";
-import Chat from "./Components/Chat/Chat";
+import Chat from "./Components/Chat/Chat.jsx";
 import Sidebar from "./Components/Sidebar";
 import { Route, Switch, BrowserRouter } from "react-router-dom";
+import Login from "./Components/Login/Login";
+import { useSelector,useDispatch } from "react-redux"
 
 function App() {
+  var data = useSelector((state) => state.PostData.UserData)
+  const user = data
   return (
     // BEM namin conventions
     <div className="app">
       <div className="app__body">
-        <BrowserRouter>
-          <Switch>
+        {!user ? (
+          <Login />
+        ) : (
+          <BrowserRouter>
+            {/* <Switch> */}
             <Sidebar />
-            <Chat />
-            <Route path="/rooms/:roomId" render={() => <Chat />} />
-            <Route path="/" extact render={() => <Chat />} />
-          </Switch>
-        </BrowserRouter>
+            {/* <Chat /> */}
+            <Route path="/rooms/:roomId">
+              <Chat />
+            </Route>
+            {/* <Route path="/" >
+              <Chat /> 
+            </Route> */}
+            {/* </Switch> */}
+          </BrowserRouter>
+        )}
+
         {/* sidebar */}
         {/* chats */}
       </div>
